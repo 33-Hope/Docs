b1dee4c9e4b01a7d9f40decfc4073691d3d56cae
33-Hope <luhuan033@gmail.com>
2025-04-06 11:31:26 +0800

    latex数学公式

--- assets/js/just-the-docs.js
+++
@@ -1,7 +0,0 @@
-lunr.tokenizer.separator = {{ site.search.tokenizer_separator | default: site.search_tokenizer_separator | default: "/[\s\-/]+/" }}
- 
-var index = lunr(function(){
-  this.use(lunr.multiLanguage('en', 'th'));
-  this.ref('id');
-  this.field('hpath', { boost: 200 });
-  this.field('title', { boost: 100 });
